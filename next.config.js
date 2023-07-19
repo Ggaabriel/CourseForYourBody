4682174a074046ab8f50505e7f6a724582bda7b8
Ggaabriel <djskilleton@gmail.com>
2023-07-19 15:28:16 +0200

    remove all

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    // Остальные настройки...
 
-    // Отключение экспериментальных картинок
-    images: {
-        disableStaticImages: true,
-    },
-    experimental: {
-        optimizeImages: false,
-    },
-    // Остальные настройки...
 };
 
 module.exports = nextConfig;
